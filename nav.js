1493d3ff0f9518a256e3db094b031ea29b6391cd
N REZA <nreza@Ns-MacBook-Pro.local>
2016-09-25 23:56:18 +0600

    added all files

---
+++ nav.js
@@ -0,0 +1,16 @@
+jQuery(function(){
+
+    jQuery('#navBttn').on('click', function() {
+      jQuery('#navBox').fadeIn(200);
+      
+    }         
+    );
+jQuery('#navBttnX').on('click', function() {
+      jQuery('#navBox').fadeOut(250);
+      
+    }         
+    );  
+    
+    
+}
+);
